f58098de9b04b3e79a08a926ba5fd7bff5365ccc
sivanbon2 <sivanbon2@gmail.com>
2021-07-10 16:41:31 +0300

    Removed the report from BuyMeTest to base page

--- src/main/java/BaseClasses/BasePage.java
+++ src/main/java/BaseClasses/BasePage.java
@@ -1,19 +1,37 @@
 package BaseClasses;
 
+import Utils.ReportUtils;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+
+import static BaseClasses.BaseWebdriver.takeScreenShot;
 
 public class BasePage {
 
     // Click on Element by one of the locators
     public By clickElement(By locator) {
-        getWebElement(locator).click();
+        try{
+         getWebElement(locator).click();
+
+        } catch (NoSuchElementException e){
+            String timeNow = String.valueOf(System.currentTimeMillis());
+            ReportUtils.test.info("Element Was Not Found", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
+        }
         return locator;
     }
 
     // SendKeys(text) to one of the locators
     public By sendKeysToElement(By locator, String text) {
+        try{
         getWebElement(locator).sendKeys(text);
+
+        }catch (NoSuchElementException e){
+            String timeNow = String.valueOf(System.currentTimeMillis());
+            ReportUtils.test.info("Element Was Not Found", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
+
+        }
         return locator;
     }
 
--- src/test/java/BuyMeTest.java
+++ src/test/java/BuyMeTest.java
@@ -1,81 +1,59 @@
 
-import Utils.ReportUtils;
 import Pages.HomePage;
 import Pages.PickBusiness;
 import Pages.SenderAndReceiverInfo;
 import Pages.SignUpPage;
-import com.aventstack.extentreports.MediaEntityBuilder;
-import com.aventstack.extentreports.Status;
+
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import static BaseClasses.BaseWebdriver.takeScreenShot;
+
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BuyMeTest {
 
     @Test
     public void test01signUp() {
-        try {
-            SignUpPage signUp = new SignUpPage();
-            signUp.completeSignUp();
-            Assert.assertEquals(signUp.getFirstName(), "Sivan");
-            Assert.assertEquals(signUp.getPassword(), "Sivan12345");
-            Assert.assertEquals(signUp.getPassword(), "Sivan12345");
-            signUp.clickOnRegistrationButton();
-            String timeNow = String.valueOf(System.currentTimeMillis());
-            ReportUtils.test.pass("The first test was pass successfully", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
-        }catch (Exception e) {
-            e.printStackTrace();
-            ReportUtils.test.log(Status.FAIL, "Registration Button was not clicked" + e.getMessage());
-        }
+
+        SignUpPage signUp = new SignUpPage();
+        signUp.completeSignUp();
+        Assert.assertEquals(signUp.getFirstName(), "Sivan");
+        Assert.assertEquals(signUp.getPassword(), "Sivan12345");
+        Assert.assertEquals(signUp.getPassword(), "Sivan12345");
+        signUp.clickOnRegistrationButton();
+
     }
 
     @Test
     public void test02homePage() {
-        try {
-            HomePage homePage = new HomePage();
-            homePage.pickFromHome();
-            assert homePage.getUrl().equals("https://buyme.co.il/search?budget=6&category=16&region=11");
-            String timeNow = String.valueOf(System.currentTimeMillis());
-            ReportUtils.test.pass("\"The second test was pass successfully\"", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
-        }catch (Exception e) {
-            e.printStackTrace();
-            ReportUtils.test.log(Status.FAIL, "Home Page URL was not found" + e.getMessage());
-        }
+        HomePage homePage = new HomePage();
+        homePage.pickFromHome();
+        assert homePage.getUrl().equals("https://buyme.co.il/search?budget=6&category=16&region=11");
+
     }
 
     @Test
     public void test03pickBusiness() {
-        try {
-            PickBusiness pickb = new PickBusiness();
-            pickb.pickABusiness();
-            assert pickb.getUrl().equals("https://buyme.co.il/money/752649?price=590");
-            String timeNow = String.valueOf(System.currentTimeMillis());
-            ReportUtils.test.pass("\"The third test was pass successfully\"", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
-        }catch (Exception e) {
-            e.printStackTrace();
-            ReportUtils.test.log(Status.FAIL, "Pick Business URL was not found" + e.getMessage());
-        }
+
+        PickBusiness pickb = new PickBusiness();
+        pickb.pickABusiness();
+        assert pickb.getUrl().equals("https://buyme.co.il/money/752649?price=590");
+
 
     }
 
     @Test
-    public void test04sendAndReceived() {
-        try {
-            SenderAndReceiverInfo sai = new SenderAndReceiverInfo();
-            sai.receiver();
-            Assert.assertEquals(sai.getReceiverName(), "Sivan");
-            sai.senderAndReceiver();
-            Assert.assertEquals(sai.getSenderName(), "Sivan ");
-            String timeNow = String.valueOf(System.currentTimeMillis());
-            ReportUtils.test.pass("\"The fourth test was pass successfully\"", MediaEntityBuilder.createScreenCaptureFromPath(takeScreenShot(timeNow)).build());
-        }catch (Exception e) {
-            e.printStackTrace();
-            ReportUtils.test.log(Status.FAIL, "" + e.getMessage());
-        }
+    public void test04sendAndReceived() throws InterruptedException {
+
+        SenderAndReceiverInfo sai = new SenderAndReceiverInfo();
+        sai.receiver();
+        Assert.assertEquals(sai.getReceiverName(), "Sivan");
+        sai.senderAndReceiver();
+        Assert.assertEquals(sai.getSenderName(), "Sivan ");
+
+
     }
 
 }
